6decc687861b7e555b51fd7edfa9298133d2e555
Brent Gardner <bgardner@squarelabs.net>
2015-08-29 14:57:34 -0600

    Formatting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var BlurShader = require('./BlurShader');
 var js2glsl = require("js2glsl");
 
-var shaderSpec = new BlurShader(); 
+var shaderSpec = new BlurShader();
 
 
 var IMG_WIDTH = 29;
@@ -19,94 +19,94 @@ var floatTexture;
 var cubeVertexPositionBuffer;
 
 function webGLStart() {
-    var pixels; 
+    var pixels;
     var testPatternButton = document.getElementById('test_pattern');
-    testPatternButton.addEventListener('click', function(e) {
-	// Test stripes for alignment
-	pixels = new Float32Array(IMG_HEIGHT*IMG_WIDTH*4);
-	pixIdx = 0;
-	for(var y = 0; y < IMG_HEIGHT; y++) {
-	    for(var x = 0; x < IMG_WIDTH; x++) {
-		var color = 0.0;
-		if(x == 0 || y == 0 || x == IMG_WIDTH-1 || y == IMG_HEIGHT-1) {
-		    color = 1.0;
-		}
-		pixels[pixIdx + 0] = color;
-		pixels[pixIdx + 1] = color;
-		pixels[pixIdx + 2] = color;
-		pixels[pixIdx + 3] = 1.0;
-		pixIdx += 4;
-	    }
-	}
+    testPatternButton.addEventListener('click', function (e) {
+        // Test stripes for alignment
+        pixels = new Float32Array(IMG_HEIGHT * IMG_WIDTH * 4);
+        pixIdx = 0;
+        for (var y = 0; y < IMG_HEIGHT; y++) {
+            for (var x = 0; x < IMG_WIDTH; x++) {
+                var color = 0.0;
+                if (x == 0 || y == 0 || x == IMG_WIDTH - 1 || y == IMG_HEIGHT - 1) {
+                    color = 1.0;
+                }
+                pixels[pixIdx + 0] = color;
+                pixels[pixIdx + 1] = color;
+                pixels[pixIdx + 2] = color;
+                pixels[pixIdx + 3] = 1.0;
+                pixIdx += 4;
+            }
+        }
 
-	draw2D(pixels);
-	var tex = createTexture(pixels, IMG_WIDTH, IMG_HEIGHT);
-	//var tmp = new Float32Array(srcImgbytes, IMG_BYTE_SZ * (imageCount-1), IMG_FLT_SZ);
-	//replaceTexture(tex, tmp, IMG_WIDTH, IMG_HEIGHT);
-	drawScene(tex, false);
-	
+        draw2D(pixels);
+        var tex = createTexture(pixels, IMG_WIDTH, IMG_HEIGHT);
+        //var tmp = new Float32Array(srcImgbytes, IMG_BYTE_SZ * (imageCount-1), IMG_FLT_SZ);
+        //replaceTexture(tex, tmp, IMG_WIDTH, IMG_HEIGHT);
+        drawScene(tex, false);
+
     });
 
     // File reader
     var fileInput = document.getElementById('fileInput');
-    fileInput.addEventListener('change', function(e) {
-	var file = fileInput.files[0];
+    fileInput.addEventListener('change', function (e) {
+        var file = fileInput.files[0];
 
-	var reader = new FileReader();
+        var reader = new FileReader();
 
-	reader.onload = function(e) {
-	    var dv = new DataView(reader.result);
-	    var pos = -4;
-	    var magic = dv.getUint32(pos += 4);
-	    console.log("magic=" + magic);
-	    
-	    // Image file
-	    if(magic == 2051) { 
-		var imageCount = dv.getUint32(pos += 4);
-		var rowCount = dv.getUint32(pos += 4);
-		var colCount = dv.getUint32(pos += 4);
-		console.log("imageCount=" + imageCount);
-		console.log("rowCount=" + rowCount);
-		console.log("colCount=" + colCount);
-		srcImgbytes = new ArrayBuffer(IMG_BYTE_SZ * imageCount);
-		pixels = new Float32Array(srcImgbytes, 0, IMG_FLT_SZ * imageCount);
-		var pixIdx = 0;
-		
-		var imgByteSize = rowCount * colCount;
-		for(var imgIdx = 0; imgIdx < imageCount; imgIdx++) {
-		    for(var y = 0; y < IMG_HEIGHT; y++) {
-			for(var x = 0; x < IMG_WIDTH; x++) {
-			    var color = 0.0;
-			    if(y < rowCount && x < colCount) {
-				color = dv.getUint8(pos + (imgIdx * imgByteSize) + (y * colCount) + x) / 255.0;
-			    }
-			    pixels[pixIdx + 0] = color;
-			    pixels[pixIdx + 1] = 0;
-			    pixels[pixIdx + 2] = 0;
-			    pixels[pixIdx + 3] = 1.0;
-			    pixIdx += 4;
-			}
-		    }
-		}
+        reader.onload = function (e) {
+            var dv = new DataView(reader.result);
+            var pos = -4;
+            var magic = dv.getUint32(pos += 4);
+            console.log("magic=" + magic);
 
-		draw2D(pixels);
-		var tex = createTexture(pixels, IMG_WIDTH, IMG_HEIGHT);
-		//var tmp = new Float32Array(srcImgbytes, IMG_BYTE_SZ * (imageCount-1), IMG_FLT_SZ);
-		//replaceTexture(tex, tmp, IMG_WIDTH, IMG_HEIGHT);
-		drawScene(tex, false);
-	    }
-	    
-	    // Label file
-	    if(magic == 2049) {
-		var lblCnt = dv.getUint32(pos += 4);
-		for(var lblIdx = 0; lblIdx < lblCnt; lblIdx++) {
-		    var lbl = dv.getUint8(pos + lblIdx);
-		    console.log("lbl=" + lbl);
-		}
-	    }
-	}
+            // Image file
+            if (magic == 2051) {
+                var imageCount = dv.getUint32(pos += 4);
+                var rowCount = dv.getUint32(pos += 4);
+                var colCount = dv.getUint32(pos += 4);
+                console.log("imageCount=" + imageCount);
+                console.log("rowCount=" + rowCount);
+                console.log("colCount=" + colCount);
+                srcImgbytes = new ArrayBuffer(IMG_BYTE_SZ * imageCount);
+                pixels = new Float32Array(srcImgbytes, 0, IMG_FLT_SZ * imageCount);
+                var pixIdx = 0;
 
-	reader.readAsArrayBuffer(file);	
+                var imgByteSize = rowCount * colCount;
+                for (var imgIdx = 0; imgIdx < imageCount; imgIdx++) {
+                    for (var y = 0; y < IMG_HEIGHT; y++) {
+                        for (var x = 0; x < IMG_WIDTH; x++) {
+                            var color = 0.0;
+                            if (y < rowCount && x < colCount) {
+                                color = dv.getUint8(pos + (imgIdx * imgByteSize) + (y * colCount) + x) / 255.0;
+                            }
+                            pixels[pixIdx + 0] = color;
+                            pixels[pixIdx + 1] = 0;
+                            pixels[pixIdx + 2] = 0;
+                            pixels[pixIdx + 3] = 1.0;
+                            pixIdx += 4;
+                        }
+                    }
+                }
+
+                draw2D(pixels);
+                var tex = createTexture(pixels, IMG_WIDTH, IMG_HEIGHT);
+                //var tmp = new Float32Array(srcImgbytes, IMG_BYTE_SZ * (imageCount-1), IMG_FLT_SZ);
+                //replaceTexture(tex, tmp, IMG_WIDTH, IMG_HEIGHT);
+                drawScene(tex, false);
+            }
+
+            // Label file
+            if (magic == 2049) {
+                var lblCnt = dv.getUint32(pos += 4);
+                for (var lblIdx = 0; lblIdx < lblCnt; lblIdx++) {
+                    var lbl = dv.getUint8(pos + lblIdx);
+                    console.log("lbl=" + lbl);
+                }
+            }
+        }
+
+        reader.readAsArrayBuffer(file);
     });
 
     // WebGL
@@ -123,29 +123,29 @@ function webGLStart() {
 }
 
 function draw2D(pixels) {
-   var canvas2d = document.getElementById("2d");
-    ctx = canvas2d.getContext("2d"); 
+    var canvas2d = document.getElementById("2d");
+    ctx = canvas2d.getContext("2d");
 
-    var w = canvas2d.width; 
+    var w = canvas2d.width;
     var h = canvas2d.height;
     var sw = 500;
-    var sh = 500; 
-    var img = ctx.createImageData(w,h); 
-    
-    shaderSpec.uniforms = {uSampler:  [pixels, IMG_WIDTH, IMG_HEIGHT] }; 
-    shaderSpec.varyings = {}; 
-    for(var x = 0;x <= sw;x++){
-	for(var y = 0;y <= sh;y++) {
-	    shaderSpec.varyings.vTextureCoord = [ x/w, y/h  ]; 
-	    var rgba = shaderSpec.FragmentColor(js2glsl.builtIns);
-	    var idx = (x + (h-y-1) * w) * 4; 
-	    for(var c = 0;c < 4;c++) {
-		img.data[idx + c] = Math.round(Math.max(0, Math.min(255, rgba[c] * 255))); 
-	    }
-	    img.data[idx + 3] = 255; 
-	}
+    var sh = 500;
+    var img = ctx.createImageData(w, h);
+
+    shaderSpec.uniforms = {uSampler: [pixels, IMG_WIDTH, IMG_HEIGHT]};
+    shaderSpec.varyings = {};
+    for (var x = 0; x <= sw; x++) {
+        for (var y = 0; y <= sh; y++) {
+            shaderSpec.varyings.vTextureCoord = [x / w, y / h];
+            var rgba = shaderSpec.FragmentColor(js2glsl.builtIns);
+            var idx = (x + (h - y - 1) * w) * 4;
+            for (var c = 0; c < 4; c++) {
+                img.data[idx + c] = Math.round(Math.max(0, Math.min(255, rgba[c] * 255)));
+            }
+            img.data[idx + 3] = 255;
+        }
     }
-    ctx.putImageData(img, 0, 0);     
+    ctx.putImageData(img, 0, 0);
 }
 
 function initGL(canvas) {
@@ -155,26 +155,26 @@ function initGL(canvas) {
 }
 
 function drawScene(texture, fbo) {
-    
-    // Optionally render to frame buffer
-    if(fbo) {
-	// Create output texture
-	var texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
 
-	// Create and attach frame buffer
-	var fbo = gl.createFramebuffer();
-	gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-	gl.bindTexture(gl.TEXTURE_2D, null);
-	if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE) {
-	    throw new Error("gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE");
-	}
+    // Optionally render to frame buffer
+    if (fbo) {
+        // Create output texture
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
+
+        // Create and attach frame buffer
+        var fbo = gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE) {
+            throw new Error("gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE");
+        }
     }
 
     // Draw the scene
@@ -188,13 +188,13 @@ function drawScene(texture, fbo) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeVertexPositionBuffer.numItems / 2 );
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeVertexPositionBuffer.numItems / 2);
 
     // http://stackoverflow.com/questions/17981163/webgl-read-pixels-from-floating-point-render-target
-    if(fbo) {
-	var pixels = new Float32Array(4 * width * height);
-	gl.readPixels(0, 0, width, height, gl.RGBA, gl.FLOAT, pixels);
-	console.log(pixels[0] + "," + pixels[1] + "," + pixels[2] + "," + pixels[3]);
+    if (fbo) {
+        var pixels = new Float32Array(4 * width * height);
+        gl.readPixels(0, 0, width, height, gl.RGBA, gl.FLOAT, pixels);
+        console.log(pixels[0] + "," + pixels[1] + "," + pixels[2] + "," + pixels[3]);
     }
 }
 
@@ -202,7 +202,7 @@ function initShaders() {
     shaderProgram = shaderSpec.GetProgram(gl)
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-	alert("Could not initialise shaders");
+        alert("Could not initialise shaders");
     }
 
     gl.useProgram(shaderProgram);
@@ -218,18 +218,18 @@ function initShaders() {
 
 function initTexture() {
     gl.getExtension('OES_texture_float');
-    
+
     // Draw texture
     var ar = new Float32Array(width * height * 4);
-    for(var i = 0; i < width * height; i++) {
-	ar[i * 4 + 0] = 0.5;
-	ar[i * 4 + 1] = 0.5;
-	ar[i * 4 + 2] = 1;
-	ar[i * 4 + 3] = 1;
+    for (var i = 0; i < width * height; i++) {
+        ar[i * 4 + 0] = 0.5;
+        ar[i * 4 + 1] = 0.5;
+        ar[i * 4 + 2] = 1;
+        ar[i * 4 + 3] = 1;
     }
 
 
-    draw2D(ar);    
+    draw2D(ar);
     // Upload texture to GPU
     floatTexture = createTexture(ar, width, height);
 }
@@ -237,15 +237,15 @@ function initTexture() {
 function replaceTexture(tex, floatAr, width, height) {
     gl.bindTexture(gl.TEXTURE_2D, tex);
     gl.texImage2D(gl.TEXTURE_2D, 	// target - Specifies the target texture
-		  0, 							// level - The level of detail value.
-		  gl.RGBA, 					// internalformat - Specifies the number of color components in the texture.
-		  width, 						// width - Width of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
-		  height, 					// height - Height of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
-		  0, 							// border - Specifies the width of the border. Must be 0. 
-		  gl.RGBA, 					// format - Contains the format for the source pixel data. Must match internalformat (see above).
-		  gl.FLOAT,					// type - The type of texture data. (gl.FLOAT creates 128 bit-per-pixel textures instead of 32 bit-per-pixel for the image)
-		  floatAr						// pixels - The ImageData array, ArrayBufferView, HTMLCanvasElement, HTMLImageElement to use as a data source for the texture.
-		 );				
+        0, 							// level - The level of detail value.
+        gl.RGBA, 					// internalformat - Specifies the number of color components in the texture.
+        width, 						// width - Width of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
+        height, 					// height - Height of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
+        0, 							// border - Specifies the width of the border. Must be 0.
+        gl.RGBA, 					// format - Contains the format for the source pixel data. Must match internalformat (see above).
+        gl.FLOAT,					// type - The type of texture data. (gl.FLOAT creates 128 bit-per-pixel textures instead of 32 bit-per-pixel for the image)
+        floatAr						// pixels - The ImageData array, ArrayBufferView, HTMLCanvasElement, HTMLImageElement to use as a data source for the texture.
+    );
     gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
@@ -254,21 +254,21 @@ function createTexture(floatAr, width, height) {
     gl.bindTexture(gl.TEXTURE_2D, tex);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 	// target - Specifies the target texture
-		  0, 							// level - The level of detail value.
-		  gl.RGBA, 					// internalformat - Specifies the number of color components in the texture.
-		  width, 						// width - Width of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
-		  height, 					// height - Height of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
-		  0, 							// border - Specifies the width of the border. Must be 0. 
-		  gl.RGBA, 					// format - Contains the format for the source pixel data. Must match internalformat (see above).
-		  gl.FLOAT,					// type - The type of texture data. (gl.FLOAT creates 128 bit-per-pixel textures instead of 32 bit-per-pixel for the image)
-		  floatAr						// pixels - The ImageData array, ArrayBufferView, HTMLCanvasElement, HTMLImageElement to use as a data source for the texture.
-		 );				
+        0, 							// level - The level of detail value.
+        gl.RGBA, 					// internalformat - Specifies the number of color components in the texture.
+        width, 						// width - Width of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
+        height, 					// height - Height of texture image. Value used only when UInt8Array or Float32Array for pixels is specified.
+        0, 							// border - Specifies the width of the border. Must be 0.
+        gl.RGBA, 					// format - Contains the format for the source pixel data. Must match internalformat (see above).
+        gl.FLOAT,					// type - The type of texture data. (gl.FLOAT creates 128 bit-per-pixel textures instead of 32 bit-per-pixel for the image)
+        floatAr						// pixels - The ImageData array, ArrayBufferView, HTMLCanvasElement, HTMLImageElement to use as a data source for the texture.
+    );
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.bindTexture(gl.TEXTURE_2D, null);
-    
+
     return tex;
 }
 
@@ -276,10 +276,10 @@ function initBuffers() {
     cubeVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
     vertices = [
-	    -1.0, -1.0,
-	     1.0, -1.0,
-	    -1.0,  1.0,
-	     1.0,  1.0,
+        -1.0, -1.0,
+        1.0, -1.0,
+        -1.0, 1.0,
+        1.0, 1.0,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     cubeVertexPositionBuffer.itemSize = 2;
